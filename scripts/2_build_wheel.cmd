6fb84c53e66ab15cff9ea03657375a7fe5a8e1f0
Alexander Sokol <sokol@compatibl.com>
2023-02-15 22:24:15 -0500

    Cleanup existing artifacts and activate venv before creating the wheel.

--- scripts/2_build_wheel.cmd
+++ scripts/2_build_wheel.cmd
@@ -1,4 +1,31 @@
+@echo off
+
+echo Switch to parent directory
 pushd ..
-pip install wheel
+echo.
+
+echo Cleanup existing artifacts
+IF EXIST "build" rd /s /q build
+IF EXIST "dist" rd /s /q dist
+IF EXIST "aenc.egg-info" rd /s /q "aenc.egg-info"
+echo.
+
+echo Activate virtual environment
+call venv\Scripts\activate.bat
+echo.
+
+echo Install setup-related packages in venv
+python -m pip install --upgrade pip
+pip install setuptools wheel
+echo.
+
+echo Create wheel
 python setup.py bdist_wheel
+echo.
+
+echo Deactivate virtual environment
+call venv\Scripts\deactivate.bat
+echo.
+
+echo Return to original directory
 popd
